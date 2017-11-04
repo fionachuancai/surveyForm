fd2a675389207c252c4e87500b0634c3310b07c5
Jiayuan Wang <fionajiayuan08@gmail.com>
2017-11-04 14:50:56 +0000

    handle the post method, and log the data to console from postman; add nodemon

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true, type: 'multipart/form-data*' }));
+
+app.use('/', (req, res, next) => {
+    console.log('Serve the survey html.');
+    next();
+});
+
+app.post('/api/submitSurvey', (req, res) => {
+    console.log(req.body);
+    res.json({});
+  }
+);
 
 app.listen(port, (error) => {
   if (error) {
