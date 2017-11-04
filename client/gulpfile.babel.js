495dafa7f668face147f9acf56f073678196c429
Jiayuan Wang <fionajiayuan08@gmail.com>
2017-11-04 15:26:16 +0000

    add gulp-watch to monitor the change and rebuild

--- client/gulpfile.babel.js
+++ client/gulpfile.babel.js
@@ -1,8 +1,23 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
+var watch = require('gulp-watch');
 
-gulp.task('default', () => {
+
+gulp.task('transform', () => {
   return gulp.src('src/**/*.js')
     .pipe(babel())
     .pipe(gulp.dest('lib'));
+});
+
+gulp.task('watch', () => {
+  return gulp.src('src/**/*.js')
+    .pipe(watch('src/**/*.js', {
+      verbose: true
+    }))
+    .pipe(babel())
+    .pipe(gulp.dest('lib'));
+});
+
+gulp.task('default', () => {
+  gulp.start('transform');
 });
